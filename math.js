e0aa41e030a5d1aea030d4fe54b3cc5c77483bd0
lauranavoni <lauranavonigomez@gmail.com>
2021-08-28 18:20:21 -0300

    changes plus

--- math.js
+++ math.js
@@ -1,13 +1,19 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10];
+const numbers = [2,4,6,8,10,12,14];
 const suma = (acc, cv) => acc + cv;
 numbers.reduce(suma);
 
 const resta = (acc, cv) => acc - cv;
 numbers.reduce(resta);
 
-const producto = (acc, cv) => acc * cv;
-numbers.reduce(producto);
+const multiplicar = (acc, cv) => acc * cv;
+numbers.reduce(multiplicar);
 
 const exp = (n) => {
   return numbers.map((number)=> number ** n);
 }
+
+let a =9
+let b =12
+let c = a+b
+console.log(c)
+
