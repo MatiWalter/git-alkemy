1ffc4c2a8b3f5dd0bb5dbab817b833289473091a
lauranavoni <lauranavonigomez@gmail.com>
2021-08-28 17:36:52 -0300

    agrego mis archivos

---
+++ archivodos.js
@@ -0,0 +1,7 @@
+var nombre ='laura';
+alert(nombre);
+
+//console.log(nombre);
+
+const nombre1='Maria';
+alert('Alumna IT: '+ nombre1);
---
+++ archivouno.js
@@ -0,0 +1,9 @@
+var edad;
+edad = 34; // tipo number
+alert('edad = '+ edad);
+edad = 'Pedro' //tipo string
+alert('edad = '+ edad);
+edad = true //tipo boolean
+alert('edad = '+ edad);
+edad = 125.6
+alert('edad = '+ edad);
